1077285648460a778befeb69a97f67447e8f3f44
Jean Moura <jeanmoura@outlook.com>
2022-09-29 19:59:05 -0300

    Adding contains duplicate sorting a vector.

---
+++ lista3/containsDuplicate.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){}
+
+class Solution {
+public:
+  bool containsDuplicate(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    for(int i = 1 ;i < nums.size(); i++)
+      if(nums[i-1]==nums[i])return true;
+      
+    return false;
+  }
+};
