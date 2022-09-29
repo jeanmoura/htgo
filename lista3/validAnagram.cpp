303cba0687bb740103c633efb26e8c8c1ed0c3c5
Jean Moura <jeanmoura@outlook.com>
2022-09-29 20:06:51 -0300

    Addig valid anagram using hashmap.

---
+++ lista3/validAnagram.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+
+
+using namespace std;
+
+int main(){}
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+    
+    unordered_map<char,int>mpp;
+    unordered_map<char,int>map;
+
+    if(s.size()!=t.size())
+        return false;
+   
+    for(int i=0;i<s.size();i++) {
+        mpp[s[i]]++;
+        map[t[i]]++;
+    }
+
+    if(mpp==map)
+        return true;
+    return false;    
+    }
+};
